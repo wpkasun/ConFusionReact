e4eba74600ff416109c944ccacc08457950e17d0
Kasun Sameera <wpkasun@gmail.com>
2023-01-15 17:02:12 +0100

    Header and Footer

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,7 +7,6 @@ import {
 function RenderDish({ dish }) {
     return (
         <div className="col-12 col-md-5 m-1">
-
             <Card>
                 <CardImg top src={dish.image} alt={dish.name} />
                 <CardBody>
